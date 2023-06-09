dfedb5d0301046bf2a83f7fa7f279efedf4fcb6c
Julin <julioleandromds12@gmail.com>
2023-06-09 17:41:58 -0300

    adicionei os butaum

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,17 @@
 <script>
-
+import Ajuda from "./Components/Ajuda.svelte";
+import Jogar from "./Components/Jogar.svelte";
+import Menu from "./Components/Menu.svelte";
+import Botao from "./Components/Menu.svelte"
+import Sobre from "./Components/Sobre.svelte";
 </script>
 
 <main>
-  
-  <!-- html -->
+  Deus e bom
+  <br><br>
+ <Botao texto = Jogar/> <br><br>
+ <Botao texto = Ajuda/> <br><br>
+ <Botao texto = Sobre/> <br><br>
 </main>
 
 <style>
--- src/Components/Menu.svelte
+++ src/Components/Menu.svelte
@@ -1,5 +1,14 @@
-<script>
+<script lang="js">
 import Jogar from "./Jogar.svelte";
 import Ajuda from "./Ajuda.svelte";
 import Sobre from "./Sobre.svelte";
+   export let texto = ""
 </script>
+
+<button class="Botao">
+    {texto}
+</button>
+
+<style>
+
+</style>
