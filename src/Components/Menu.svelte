5326675d7febd81624bfb1a551ff0eb221e4b153
Emanoel Carvalho <ehcs@dicente.ifpe.edu.br>
2023-06-15 16:01:33 -0300

    Ajustes no sobre

--- src/Components/Menu.svelte
+++ src/Components/Menu.svelte
@@ -5,38 +5,56 @@
     import Jogar from "./Jogar.svelte";
 </script>
 
-<div class="div-posicao"> <!--serve para mudar a posição do botão-->
+<main>
+    <h1 class="titulo">Códigos Celestiais</h1>
+    <!--titulo do jogo-->
 
-    <button class="jogar" on:click={() => trocadeestado("jogar")}> 
-        Jogar
+    <div class="div-posicao">
+        <!--serve para mudar a posição do botão-->
+
+        <button class="jogar" on:click={() => trocadeestado("jogar")}>
+            Jogar
         </button>
 
-    <button class="ajuda" on:click={() => trocadeestado("ajuda")}> <!--tag em html que serve para criar os botões de acordo com o nome-->
-        Ajuda
-       </button>
-       
-    <button class="sobre" on:click={() => trocadeestado("sobre")}> 
-        Sobre
-    </button>
-       
-       
-</div>
+        <button class="ajuda" on:click={() => trocadeestado("ajuda")}>
+            <!--tag em html que serve para criar os botões de acordo com o nome-->
+            Ajuda
+        </button>
+
+        <button class="sobre" on:click={() => trocadeestado("sobre")}>
+            Sobre
+        </button>
+    </div>
+</main>
 
 <style>
+    .titulo {
+        /*chamando a classe do titulo**/
+        text-align: center; /*alinhamento do texto**/
+        font-size: 50px; /*tamanho da fonte**/
+        color: #810785; /*cor da fonte**/
+        font-family: "OCR A Std, monospace"; /*fonte**/
+        margin-bottom: 50px; /*margem do titulo**/
+        top: 10px; /*posição do titulo**/ 
+    }
+    .div-posicao button {
+        /*chamando a div da posição dos botões**/
+        display: block; /**bloco*/
+        width: 100%; /*margem do botão**/
+        margin-bottom: 20px;
+    }
 
-.div-posicao button{ /*chamando a div da posição dos botões**/
-    display: block; /**bloco*/
-    width: 100%; /*margem do botão**/
-    margin-bottom: 20px; 
-}
+    
+    .jogar {
+        background-color: lightsteelblue;
+    }
 
-.jogar{background-color:lightsteelblue;}
+    .sobre {
+        background-color: lightsteelblue;
+    }
 
-.sobre {background-color:lightsteelblue;}
-
-.ajuda {background-color:lightsteelblue;}
+    .ajuda {
+        background-color: lightsteelblue;
+    }
 
 </style>
-
-
-
---
+++ src/Components/Sobre.svelte
@@ -0,0 +1,70 @@
+<script>
+    import { trocadeestado } from "../stores/estado";
+</script>
+
+<main>
+    <div class="sobre">
+        <h2>Sobre: Jogo Rpg Tático (Caçadores de Sombras)</h2>
+        <p>
+            Este projeto de RPG Tático foi desenvolvido como parte do trabalho de conclusão da disciplina 
+            de Lógica de Programação e Estrutura de Dados. 
+            O jogo foi desenvolvido com o objetivo de proporcionar uma
+            experiência envolvente e estratégica para os fãs da série de livros
+            "Instrumentos Mortais" (Shadowhunters) escrita por Cassandra Clare.
+        </p>
+        <p>
+            Neste jogo, os jogadores assumem o papel de caçadores de sombras e
+            enfrentam desafios táticos em batalhas estratégicas contra demônios,
+            vampiros, lobisomens e outras criaturas sobrenaturais. Os jogadores
+            devem usar suas habilidades, magias e armas para proteger o mundo
+            dos mundanos e manter o equilíbrio entre a luz e as sombras.
+        </p>
+        
+        <p>
+            Para mais informações sobre o projeto, acesse nosso repositório no GitHub:
+            <a href="https://github.com/JulioLeandro12/Projeto_Game.git" >
+         Github
+        </a>
+        </p>
+     
+        
+
+        <button class="voltar" on:click={() => trocadeestado("menu")}>
+            Voltar
+        </button>
+    </div>
+</main>
+
+<style>
+    button {
+        background-color: lightsteelblue;
+        position: fixed;
+        top: 10px;
+        left: 15px;
+        height: auto;
+        width: 150px;
+    }
+
+  .sobre {
+    text-align: center;
+    margin-top: 50px;
+    font-family: Arial, sans-serif;
+    color: #700476;
+    font-size: 20px;
+    box-shadow: 0 2px 4px rgba(249, 248, 248, 0.1);
+    border: 1px solid #e7e3e3
+  }
+
+  p {
+    margin-bottom: 20px; /* Margem inferior */
+    line-height: 2; /* Altura da linha */
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif
+  }
+  h2 {
+    font-size: 44px;
+    margin-bottom: 20px;
+
+  }
+
+</style>
+
