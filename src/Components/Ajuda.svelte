7ee18b70816b3bc94b1c475596ceb07a42737035
Julin <julioleandromds12@gmail.com>
2023-06-16 16:21:33 -0300

    botao voltar, ajuda, aprimoramento de sobre e ajuda

---
+++ src/Components/Ajuda.svelte
@@ -0,0 +1,67 @@
+<script>
+    import {trocadeestado} from "../stores/estado";
+    
+</script>
+
+<main>
+    <div class = "ajuda">
+        <h1>Como jogar:</h1>
+        <h2>Usando as setas do teclado
+            <br><br>
+            <div class ="setinhas"> 
+                <img src="..\..\public\imagens\Setinhas150-97.png" alt="Setinhas">
+            </div>
+            <br>
+            Selecione o personagem com a <br> tecla Enter
+            <br><br>
+            <div class = "enter">
+                <img src="../../public/imagens/Enter150-150.png" alt="Enter">
+            </div>
+            <br>
+
+            Escolha a ação do seu personagem.<br><br>
+            Good Game!!
+        </h2>
+    </div>
+    <div> 
+        <button class= "voltar" on:click={()=> trocadeestado ("menu")}>
+        </button>
+    </div>
+</main>
+
+
+<style>
+    button {
+      background-image: url(../../public/imagens/voltar_botao.png) no repeat;
+      background-size: cover;
+      padding: auto;
+      height: 20px;
+      width: 100px;
+      border-radius: 15px;
+
+
+    }
+    .ajuda{
+        background-color: #000000aa ;
+        text-align: center;
+        margin-top: 30px;
+        margin-bottom: 20px;
+        margin-left: 20px;
+        margin-right: 20px;
+        font-family: inherit;
+        font-size: 20px;
+        color: rebeccapurple;
+        box-shadow: 0 2px 4px rgba(249, 248, 248, 0.1);
+        border:3px solid #39393984
+        
+
+    }
+    
+    .voltar {
+        background-image: url(../../public/imagens/voltar_botao.png);
+        background-size: cover;
+    }
+
+
+    
+</style>
--- src/Components/Sobre.svelte
+++ src/Components/Sobre.svelte
@@ -4,19 +4,19 @@
 
 <main>
     <div class="sobre">
-        <h2>Sobre: Jogo Rpg Tático (Caçadores de Sombras)</h2>
+        <h2>Sobre o Jogo Rpg Tático (Códigos Celestiais):</h2>
         <p>
             Este projeto de RPG Tático foi desenvolvido como parte do trabalho de conclusão da disciplina 
-            de Lógica de Programação e Estrutura de Dados. 
+            de br Lógica de Programação e Estrutura de Dados. 
             O jogo foi desenvolvido com o objetivo de proporcionar uma
             experiência envolvente e estratégica para os fãs da série de livros
             "Instrumentos Mortais" (Shadowhunters) escrita por Cassandra Clare.
         </p>
         <p>
             Neste jogo, os jogadores assumem o papel de caçadores de sombras e
-            enfrentam desafios táticos em batalhas estratégicas contra demônios,
+            enfrentam desafios táticos em batalhas estratégicas <br> contra demônios,
             vampiros, lobisomens e outras criaturas sobrenaturais. Os jogadores
-            devem usar suas habilidades, magias e armas para proteger o mundo
+            devem usar suas habilidades, <br> magias e armas para proteger o mundo
             dos mundanos e manter o equilíbrio entre a luz e as sombras.
         </p>
         
@@ -26,33 +26,36 @@
          Github
         </a>
         </p>
-     
-        
-
-        <button class="voltar" on:click={() => trocadeestado("menu")}>
-            Voltar
-        </button>
     </div>
+    <div><button class="voltar" on:click={() => trocadeestado("menu")}>
+    </button></div>
 </main>
 
 <style>
     button {
-        background-color: lightsteelblue;
-        position: fixed;
-        top: 10px;
-        left: 15px;
-        height: auto;
-        width: 150px;
+      background-image: url(../../public/imagens/voltar_botao.png) no repeat;
+      background-size: cover;
+      padding: auto;
+      height: 20px;
+      width: 100px;
+      border-radius: 15px;
+    }
+    .voltar {
+      background-image: url(../../public/imagens/voltar_botao.png);
+      background-size: cover;
     }
 
   .sobre {
+    background-color: #000000aa;
     text-align: center;
-    margin-top: 50px;
-    font-family: Arial, sans-serif;
-    color: #700476;
-    font-size: 20px;
+    margin-top: 30px;
+    margin-bottom: 20px;
+    font-family: inherit, sans-serif;
+    color: rebeccapurple;
+    font-size: 25px;
     box-shadow: 0 2px 4px rgba(249, 248, 248, 0.1);
-    border: 1px solid #e7e3e3
+    border: 3px solid #39393984
+    
   }
 
   p {
